9f46b0e47e80d688abccaf098e0b3a9439a846b3
Patricia Sanchez <patsanchezmessa@gmail.com>
2023-10-11 12:34:32 -0300

    github-favorites

---
+++ js/main.js
@@ -0,0 +1,22 @@
+import { FavoritesView } from "./Favorites.js";
+
+new FavoritesView("#app");
+
+// class Pessoa {
+//   nome;
+//   sobrenome;
+//   idade;
+
+//   constructor(nome1, sobrenome1, idade1) {
+//     this.nome = nome1;
+//     this.sobrenome = sobrenome1;
+//     this.idade = idade1;
+//   }
+
+//   exibirNomeCompleto() {
+//     console.log(this.nome + " " + this.sobrenome);
+//   }
+// }
+
+// const p = new Pessoa("Patricia", "Sanchez", 33);
+// console.log(p.nome);
